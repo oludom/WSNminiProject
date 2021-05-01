52559647ae07c99e316cb665558ad3b5f6b703ac
Jojooo0o <jonasdorsch@web.de>
2021-05-01 13:00:27 +0200

    not really working as intended but not wrong :D

--- broadcasting_node.c
+++ broadcasting_node.c
@@ -15,7 +15,9 @@
 #define LOG_LEVEL LOG_LEVEL_DBG
 
 /*---------------------------------------------------------------------------*/
-#define SEND_INTERVAL (8 * CLOCK_SECOND)
+#define SEND_INTERVAL (4 * CLOCK_SECOND)
+
+static unsigned int current_channel = 11;
 
 #if MAC_CONF_WITH_TSCH
 #include "net/mac/tsch/tsch.h"
@@ -33,63 +35,77 @@ AUTOSTART_PROCESSES(&broadcasting_node_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(broadcasting_node_process, ev, data)
 {
-    static struct etimer periodic_timer;
-    static unsigned count = 0;
+  static struct etimer periodic_timer;
+  // static unsigned count = 0;
 
-    PROCESS_BEGIN();
+  PROCESS_BEGIN();
 
-    cc2420_set_channel(11);
+  cc2420_set_channel(current_channel);
 
-    #if MAC_CONF_WITH_TSCH
-    tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-    #endif /* MAC_CONF_WITH_TSCH */
+  #if MAC_CONF_WITH_TSCH
+  tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
+  #endif /* MAC_CONF_WITH_TSCH */
 
-    /* Initialize NullNet */
-    nullnet_buf = (uint8_t *)&count;
-    nullnet_len = sizeof(count);
-    nullnet_set_input_callback(input_callback);
+  /* Initialize NullNet */
+  nullnet_buf = (uint8_t *)&current_channel;
+  nullnet_len = sizeof(current_channel);
+  nullnet_set_input_callback(input_callback);
 
-    etimer_set(&periodic_timer, SEND_INTERVAL);
-    while(1)
-    {
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  while(1)
+  {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    LOG_INFO("Sending %u to ", current_channel);
+    LOG_INFO_LLADDR(NULL);
+    LOG_INFO_("\n");
 
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-        LOG_INFO("Sending %u to ", count);
-        LOG_INFO_LLADDR(NULL);
-        LOG_INFO_("\n");
+    memcpy(nullnet_buf, &current_channel, sizeof(current_channel));
+    nullnet_len = sizeof(current_channel);
 
-        memcpy(nullnet_buf, &count, sizeof(count));
-        nullnet_len = sizeof(count);
+    NETSTACK_NETWORK.output(NULL);
 
-        NETSTACK_NETWORK.output(NULL);
+    etimer_reset(&periodic_timer);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
-        count++;
-        etimer_reset(&periodic_timer);
+    if(current_channel < 13){
+      current_channel += 1;
+    }else{
+      current_channel = 11;
     }
+    cc2420_set_channel(current_channel);
 
-    PROCESS_END();
+    etimer_reset(&periodic_timer);
+  }
+
+  PROCESS_END();
 }
 
 /*---------------------------------------------------------------------------*/
 void printFloat(char* string,float F)
 {
-    unsigned long A;
-    float frac;
+  unsigned long A;
+  float frac;
 
-    A=F;
-    frac=(F-A)*pow(10,6);
-    LOG_INFO("%s%lu.%06lu\n",string,A,(unsigned long)frac);
+  A=F;
+  frac=(F-A)*pow(10,6);
+  LOG_INFO("%s%lu.%06lu\n",string,A,(unsigned long)frac);
 }
 
 /*---------------------------------------------------------------------------*/
 void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest)
 {
-    if(len == sizeof(unsigned))
-    {
-        unsigned count;
-        memcpy(&count, data, sizeof(count));
-        LOG_INFO("Received %u from ", count);
-        LOG_INFO_LLADDR(src);
-        LOG_INFO_("\n");
+  if(len == sizeof(unsigned))
+  {
+    unsigned recv_channel;
+    memcpy(&recv_channel, data, sizeof(recv_channel));
+    if(recv_channel != current_channel){
+      LOG_INFO("Channel not matching anymore: %u ", recv_channel);
+      LOG_INFO_LLADDR(src);
+      LOG_INFO_("\n");
+    } else {
+      LOG_INFO("Current Channel %u from ", recv_channel);
+      LOG_INFO_LLADDR(src);
+      LOG_INFO_("\n");
     }
+  }
 }
