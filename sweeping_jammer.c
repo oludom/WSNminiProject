3b011b06c94e2bbccc0f3b3949782dd9775e8b51
Balázs Molnár <molnbalazs97@gmail.com>
2021-05-14 00:21:17 +0200

    rapid sweeping jams

--- sweeping_jammer.c
+++ sweeping_jammer.c
@@ -8,93 +8,39 @@
 PROCESS(sweeping_jammer, "jammer");
 AUTOSTART_PROCESSES(&sweeping_jammer);
 
-#define NUMBER_CHANNELS 8
-
-int avg(int values[], int length){
-  int i, retVal = 0; 
-  for (i = 0; i<length; i++){
-    retVal += values[i];
-  }
-  return (int)(retVal/length);
-}
-
+#define NUMBER_CHANNELS 16
 
 PROCESS_THREAD(sweeping_jammer, ev, data)
 {
 
     static struct etimer timer;
-    static int channelNumber = 11;
-    static int value = 0;
-    static int rssivalues[NUMBER_CHANNELS][4] = {{0}};
-    static int i = 0;
-    static int j = 0;
-    static int minChannelValue = -100, minChannelNumber = 11;
+    static int channel, txpower = -25;
 
     PROCESS_BEGIN();
     NETSTACK_RADIO.init();
-    etimer_set(&timer, (32)); // 1/4s time interval
+    NETSTACK_RADIO.on();
 
+    // set channel power
+    NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, txpower);
 
-    // set channel
-    NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, (radio_value_t) channelNumber);
+    etimer_set(&timer, (10));
 
     while(1) {
+        leds_toggle(LEDS_GREEN);
+
+        for(channel = 11; channel <= 26; channel++)
+        {
+            NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, channel);
+
+            // carrier frequency strobe
+            NETSTACK_RADIO.set_value(RADIO_PARAM_POWER_MODE, RADIO_POWER_MODE_CARRIER_ON);
+            printf("carrier on.\n");
+        }
 
         // wait for timer
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
         etimer_reset(&timer);
-
-        // NETSTACK_RADIO.get_value(RADIO_PARAM_RSSI, &value);
-
-        leds_toggle(LEDS_GREEN);
-
-        if(i<32){
-            // sense
-            NETSTACK_RADIO.get_value(RADIO_PARAM_RSSI, &value);
-
-            printf("channel: %d; rssi param: %d\n", channelNumber, (int) value);
-            rssivalues[(int)((channelNumber-11)/2)][i%4] = (int)value;
-
-            j++;
-            if(j>3){
-                
-                // calc average
-                int a = avg(rssivalues[(int)((channelNumber-11)/2)], 4);
-                if (a > minChannelValue){
-                    minChannelValue = a;
-                    minChannelNumber = channelNumber;
-                    printf("average: %d; channel: %d\n", a, minChannelNumber);
-                }
-
-                channelNumber +=2;
-                j = 0;
-                // set channel
-                NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, (radio_value_t) channelNumber);
-            }
-
-        }
-        else if (i==32){
-            // set channel
-            printf("set channel to %d\n", minChannelNumber);
-            NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, (radio_value_t) minChannelNumber);
-            minChannelNumber = 11;
-            minChannelValue = -100;
-
-        } else {
-
-            // jam 
-
-            NETSTACK_RADIO.set_value(RADIO_PARAM_POWER_MODE, RADIO_POWER_MODE_CARRIER_ON);
-            printf("carrier on.\n");
-        }
-        i++;
-        if(i>=272){
-            i = 0;
-            channelNumber = 11;
-        }
-
     }
 
     PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
